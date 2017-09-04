a1d845c671af2bcbcdc823edb79f29818b3ac75a
Philipp Hager <office@phager.at>
2017-09-04 09:10:53 +0200

    bump version number to 2017081001 (release v3.3.1) and update CHANGELOG

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017081000;
+$plugin->version   = 2017081001;
 $plugin->requires  = 2017051500;
 $plugin->component = 'mod_publication';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "v3.3.0";
+$plugin->release   = "v3.3.1";
