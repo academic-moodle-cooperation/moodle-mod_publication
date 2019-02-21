119d26b950049b88b3eae2f2bd5ecfdd43aa0532
Philipp Hager <philipp.hager@tuwien.ac.at>
2019-02-21 13:57:02 +0100

    #6025 reorganize PHPUnit tests

--- tests/allfilestable_test.php
+++ tests/allfilestable_test.php
@@ -22,6 +22,11 @@
  * @copyright 2017 Academic Moodle Cooperation {@link http://www.academic-moodle-cooperation.org}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+namespace mod_publication\local\tests;
+
+use Exception;
+use mod_assign_generator;
+use coding_exception;
 
 if (!defined('MOODLE_INTERNAL')) {
     die('Direct access to this script is forbidden.'); // It must be included from a Moodle page!
@@ -39,119 +44,10 @@ require_once($CFG->dirroot . '/mod/publication/locallib.php'); // Include the co
  * @copyright 2017 Academic Moodle Cooperation {@link http://www.academic-moodle-cooperation.org}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class publication_allfilestable_test extends advanced_testcase {
-    /** Default number of students to create */
-    const DEFAULT_STUDENT_COUNT = 50;
-    /** Default number of teachers to create */
-    const DEFAULT_TEACHER_COUNT = 6;
-    /** Default number of editing teachers to create */
-    const DEFAULT_EDITING_TEACHER_COUNT = 6;
-    /** Number of timestamps to create */
-    const DEFAULT_TIMESTAMP_COUNT = 6;
-    /** Number of groups to create */
-    const GROUP_COUNT = 6;
-
-    /** @var stdClass $course New course created to hold the assignments */
-    protected $course = null;
-
-    /** @var array $teachers List of DEFAULT_TEACHER_COUNT teachers in the course */
-    protected $teachers = null;
-
-    /** @var array $editingteachers List of DEFAULT_EDITING_TEACHER_COUNT editing teachers in the course */
-    protected $editingteachers = null;
-
-    /** @var array $students List of DEFAULT_STUDENT_COUNT students in the course */
-    protected $students = null;
-
-    /** @var array $groups List of 10 groups in the course */
-    protected $groups = null;
-
-    /** @var array $timestamps List of 10 different timestamps */
-    protected $timestamps = null;
-
-    /**
-     * Setup function - we will create a course and add an tmt instance to it.
+class allfilestable_testcase extends base {
+    /*
+     * The base test class already contains a setUp-method setting up a course including users and groups.
      */
-    protected function setUp() {
-        global $DB;
-
-        $this->resetAfterTest(true);
-
-        $this->course = self::getDataGenerator()->create_course();
-        $this->teachers = [];
-        for ($i = 0; $i < self::DEFAULT_TEACHER_COUNT; $i++) {
-            array_push($this->teachers, self::getDataGenerator()->create_user());
-        }
-
-        $this->editingteachers = [];
-        for ($i = 0; $i < self::DEFAULT_EDITING_TEACHER_COUNT; $i++) {
-            array_push($this->editingteachers, self::getDataGenerator()->create_user());
-        }
-
-        $this->students = [];
-        for ($i = 0; $i < self::DEFAULT_STUDENT_COUNT; $i++) {
-            array_push($this->students, self::getDataGenerator()->create_user());
-        }
-
-        $this->groups = [];
-        for ($i = 0; $i < self::GROUP_COUNT; $i++) {
-            array_push($this->groups, self::getDataGenerator()->create_group(['courseid' => $this->course->id]));
-        }
-
-        $this->timestamps = [];
-        for ($i = 0; $i < self::DEFAULT_TIMESTAMP_COUNT; $i++) {
-            $hour = rand(0, 23);
-            $minute = rand(0, 60);
-            $second = rand(0, 60);
-            $month = rand(1, 12);
-            $day = rand(0, 31);
-            $year = rand(1980, date('Y'));
-            array_push($this->timestamps, mktime($hour, $minute, $second, $month, $day, $year));
-        }
-
-        $teacherrole = $DB->get_record('role', ['shortname' => 'teacher']);
-        foreach ($this->teachers as $i => $teacher) {
-            self::getDataGenerator()->enrol_user($teacher->id,
-                    $this->course->id,
-                    $teacherrole->id);
-            groups_add_member($this->groups[$i % self::GROUP_COUNT], $teacher);
-        }
-
-        $editingteacherrole = $DB->get_record('role', ['shortname' => 'editingteacher']);
-        foreach ($this->editingteachers as $i => $editingteacher) {
-            self::getDataGenerator()->enrol_user($editingteacher->id,
-                    $this->course->id,
-                    $editingteacherrole->id);
-            groups_add_member($this->groups[$i % self::GROUP_COUNT], $editingteacher);
-        }
-
-        $studentrole = $DB->get_record('role', ['shortname' => 'student']);
-        foreach ($this->students as $i => $student) {
-            self::getDataGenerator()->enrol_user($student->id,
-                    $this->course->id,
-                    $studentrole->id);
-            groups_add_member($this->groups[$i % self::GROUP_COUNT], $student);
-        }
-
-        // Make sure to run these tests as (editing)teacher, due to students getting no table if no files are present!
-        self::setUser($this->editingteachers[0]);
-    }
-
-    /**
-     * Convenience function to create a testable instance of a publication instance.
-     *
-     * @param array $params Array of parameters to pass to the generator
-     * @return testable_publication Testable wrapper around the publication class.
-     */
-    protected function create_instance($params = []) {
-        $generator = self::getDataGenerator()->get_plugin_generator('mod_publication');
-        $params['course'] = $this->course->id;
-        $instance = $generator->create_instance($params);
-        $cm = get_coursemodule_from_instance('publication', $instance->id);
-        $context = context_module::instance($cm->id);
-
-        return new testable_publication($cm, $this->course, $context);
-    }
 
     /**
      * Tests the basic creation of a publication instance with standardized settings!
@@ -162,6 +58,8 @@ class publication_allfilestable_test extends advanced_testcase {
 
     /**
      * Tests if we can create an allfilestable without uploaded files
+     *
+     * @throws Exception
      */
     public function test_allfilestable_upload() {
         // Setup fixture!
@@ -172,15 +70,11 @@ class publication_allfilestable_test extends advanced_testcase {
         ]);
 
         // Exercise SUT!
-        try {
-            ob_start();
-            $publication->display_allfilesform();
-            $output = ob_get_contents();
-            ob_end_clean();
-            self::assertFalse(strpos($output, "Nothing to display"));
-        } catch (Exception $e) {
-            throw $e;
-        }
+        ob_start();
+        $publication->display_allfilesform();
+        $output = ob_get_contents();
+        ob_end_clean();
+        self::assertFalse(strpos($output, "Nothing to display"));
 
         // Teardown fixture!
         $publication = null;
@@ -188,9 +82,12 @@ class publication_allfilestable_test extends advanced_testcase {
 
     /**
      * Tests if we can create an allfilestable without imported files
+     *
+     * @throws coding_exception
      */
     public function test_allfilestable_import() {
         // Setup fixture!
+        /** @var mod_assign_generator $generator */
         $generator = self::getDataGenerator()->get_plugin_generator('mod_assign');
         $params['course'] = $this->course->id;
         $assign = $generator->create_instance($params);
@@ -202,15 +99,11 @@ class publication_allfilestable_test extends advanced_testcase {
         ]);
 
         // Exercise SUT!
-        try {
-            ob_start();
-            $publication->display_allfilesform();
-            $output = ob_get_contents();
-            ob_end_clean();
-            self::assertFalse(strpos($output, "Nothing to display"));
-        } catch (Exception $e) {
-            throw $e;
-        }
+        ob_start();
+        $publication->display_allfilesform();
+        $output = ob_get_contents();
+        ob_end_clean();
+        self::assertFalse(strpos($output, "Nothing to display"));
 
         // Teardown fixture!
         $publication = null;
@@ -218,9 +111,12 @@ class publication_allfilestable_test extends advanced_testcase {
 
     /**
      * Tests if we can create an allfilestable without imported group-files
+     *
+     * @throws coding_exception
      */
     public function test_allfilestable_group() {
         // Setup fixture!
+        /** @var \mod_assign_generator $generator */
         $generator = self::getDataGenerator()->get_plugin_generator('mod_assign');
         $params['course'] = $this->course->id;
         $params['teamsubmission'] = 1;
@@ -232,29 +128,13 @@ class publication_allfilestable_test extends advanced_testcase {
         ]);
 
         // Exercise SUT!
-        try {
-            ob_start();
-            $publication->display_allfilesform();
-            $output = ob_get_contents();
-            ob_end_clean();
-            self::assertFalse(strpos($output, "Nothing to display"));
-        } catch (Exception $e) {
-            throw $e;
-        }
+        ob_start();
+        $publication->display_allfilesform();
+        $output = ob_get_contents();
+        ob_end_clean();
+        self::assertFalse(strpos($output, "Nothing to display"));
 
         // Teardown fixture!
         $publication = null;
     }
-
-}
-
-/**
- * Test subclass that makes all the protected methods we want to test public.
- *
- * @package   mod_publication
- * @author    Philipp Hager
- * @copyright 2017 Academic Moodle Cooperation {@link http://www.academic-moodle-cooperation.org}
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-class testable_publication extends publication {
 }
