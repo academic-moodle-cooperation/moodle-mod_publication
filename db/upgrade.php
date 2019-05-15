54a0db14a9e2a4fa08306897bc0ca6e1afc70d44
Hannes Laimer <hannes.laimer@rolmail.net>
2019-05-15 09:37:35 +0200

    upgrade.ph restructured

--- db/upgrade.php
+++ db/upgrade.php
@@ -173,25 +173,14 @@ function xmldb_publication_upgrade($oldversion) {
         // Define field notifyteacher to be added to publication.
         $table = new xmldb_table('publication');
         $field = new xmldb_field('notifyteacher', XMLDB_TYPE_INTEGER, '2', null, XMLDB_NOTNULL, null, '1', 'groupapproval');
+        $field2 = new xmldb_field('notifystudents', XMLDB_TYPE_INTEGER, '2', null, XMLDB_NOTNULL, null, '0', 'notifyteacher');
 
         // Conditionally launch add field notifyteacher.
         if (!$dbman->field_exists($table, $field)) {
             $dbman->add_field($table, $field);
         }
-
-        // Publication savepoint reached.
-        upgrade_mod_savepoint(true, 2019020101, 'publication');
-    }
-
-    if ($oldversion < 2019020101) {
-
-        // Define field notifystudents to be added to publication.
-        $table = new xmldb_table('publication');
-        $field = new xmldb_field('notifystudents', XMLDB_TYPE_INTEGER, '2', null, XMLDB_NOTNULL, null, '0', 'notifyteacher');
-
-        // Conditionally launch add field notifystudents.
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
+        if (!$dbman->field_exists($table, $field2)) {
+            $dbman->add_field($table, $field2);
         }
 
         // Publication savepoint reached.
