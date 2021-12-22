7e2abeec882197c5e95a51b89177485b3cd334a8
Simeon Naydenov <moniNaydenov@gmail.com>
2021-12-22 14:23:10 +0200

    fix codechecker again

--- amd/src/alignrows.js
+++ amd/src/alignrows.js
@@ -17,7 +17,6 @@
  * Javascript to align rows
  *
  * @module    mod_publication/alignrows
- * @package
  * @author    Hannes Laimer
  * @copyright 2020 Academic Moodle Cooperation {@link http://www.academic-moodle-cooperation.org}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
